ef7f3c087bcf98557348e4aeb443522f7bdef0ce
Henry Bust <henry.b@SCHULEN-BN.DE>
2020-09-09 12:22:50 +0200

    added missing Bruch.cs

--- Bruch/Modelle/Bruch.cs
+++ Bruch/Modelle/Bruch.cs
@@ -10,5 +10,59 @@ namespace Bruch
 {
     class Bruch
     {
+        //Attribute
+        private int zaehler;
+        private int nenner;
+
+
+        //Konstruktoren
+        public Bruch(int zaehler, int nenner)
+        {
+            this.setZaehler(zaehler);
+            this.setNenner(nenner);
+        }
+
+
+        //Selektoren
+        public int getZaehler()
+        {
+            return zaehler;
+        }
+        public void setZaehler(int zaehler)
+        {
+            this.zaehler = zaehler;
+        }
+        public int getNenner()
+        {
+            return nenner;
+        }
+        public void setNenner(int nenner)
+        {
+            this.nenner = nenner;
+        }
+
+        //Funktionen
+        public void addieren(Bruch bruch)
+        {
+
+        }
+
+        public void subtrahieren(Bruch bruch)
+        {
+
+        }
+
+        public void dividieren(Bruch bruch)
+        {
+
+        }
+        public void multiplizieren(Bruch bruch)
+        {
+
+        }
+        public void kuerzen()
+        {
+
+        }
     }
 }
